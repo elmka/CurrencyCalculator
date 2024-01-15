b1e58028a792b055db92d9b8825fa4503ce15def
elmka <123328478+elmka@users.noreply.github.com>
2024-01-15 20:29:37 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState(0);
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(0);
+
+  function calculate() {
+    if (age && age >= 0 && age <= 220) {
+      const a = age.replace(',', '.');
+      setMin((220 - a) * 0.65);
+      setMax((220 - a) * 0.85);
+    }
+    else {
+      setMin(0);
+      setMax(0);
+    }
+  }
+
+  useEffect(() => {
+    calculate();
+  }, [age]);
+
+  const txt = age && age >= 0 && age <= 220
+    ? `${min.toFixed(0)}-${max.toFixed(0)}`
+    : 'Please enter a number between 0 and 220.';
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Age</Text>
+      <TextInput style={styles.field} value={age} onChangeText={text => setAge(text)} keyboardType='decimal-pad' />
+      <Text style={styles.field}>Limits</Text>
+      <Text style={styles.field}>{age ? txt : ''}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 10
   },
+  field: {
+    marginBottom: 10
+  }
 });
